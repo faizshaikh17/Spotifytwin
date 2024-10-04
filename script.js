6b9477f292194a293767ce16a702e1ac6f90570a
Faizz <119841230+faizshaikh17@users.noreply.github.com>
2024-10-04 12:09:38 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector(".input-bar");
+
+input.addEventListener("click",()=>{
+    let section2 = document.querySelector(".section2")
+    let section2head = document.querySelector(".section2-head");
+    let section2artist = document.querySelector(".section2-artist")
+
+    section2.classList.replace(".section3")
+})
